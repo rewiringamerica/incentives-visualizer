6f6b24da728f7b9132ddc8aeae9cdeab408f33af
Emma Tong <53305901+etong11@users.noreply.github.com>
2025-02-13 02:16:16 -0500

    feat: set up state layer with tooltip/popup

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -17,6 +17,39 @@ const Map: React.FC = () => {
       zoom: 4,
     });
 
+    map.on('load', () => {
+      // Source for state polygons, can be replaced with a different source.
+      map.addSource('states', {
+          'type': 'geojson',
+          'data': 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces_shp.geojson'
+      });
+
+      map.addLayer({
+          'id': 'states-layer',
+          'type': 'fill',
+          'source': 'states',
+          'paint': {
+              'fill-color': 'rgba(249, 214, 91)',
+              'fill-outline-color': 'rgb(0, 0, 0)'
+          }
+      });
+
+      map.on('click', 'states-layer', (e) => {
+          new maplibregl.Popup()
+              .setLngLat(e.lngLat)
+              .setHTML(e.features && e.features[0] ? e.features[0].properties.name : 'Could not load data')
+              .addTo(map);
+      });
+
+      // Adjust the cursor to a pointer when it is over the states layer.
+      map.on('mouseenter', 'states-layer', () => {
+          map.getCanvas().style.cursor = 'pointer';
+      });
+      map.on('mouseleave', 'states-layer', () => {
+          map.getCanvas().style.cursor = '';
+      });
+    });
+
     return () => map.remove();
   }, []);
 
