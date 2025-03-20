e550ec569235e4d2c310357b4d8d418a69c3d6eb
Albert Lin <albert1104@gmail.com>
2025-03-20 05:39:48 -0400

    fix: legend shows up again on load

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../styles/index.css';
 import Legend from './Legend';
 import { loadStates, StateData } from './States';
@@ -11,11 +11,13 @@ interface MapProps {
 
 const Map: React.FC<MapProps> = ({ onStateSelect }) => {
   const mapContainer = useRef<HTMLDivElement | null>(null);
-  const mapRef = useRef<maplibregl.Map | null>(null);
+  const [mapInstance, setMapInstance] = useState<maplibregl.Map | null>(null);
   const STYLE_VERSION = 8; // Required for declaring a style, may change in the future
 
   useEffect(() => {
-    if (!mapContainer.current) {return;}
+    if (!mapContainer.current) {
+      return;
+    }
     const API_KEY = process.env.MAPTILER_API_KEY;
     const map = new maplibregl.Map({
       container: mapContainer.current,
@@ -36,20 +38,16 @@ const Map: React.FC<MapProps> = ({ onStateSelect }) => {
     map.on('load', () => {
       // Load states and pass the onStateSelect callback so a state click will notify the parent.
       loadStates(map, onStateSelect);
-      setMapStyle(map.getStyle());
-      // Need to expose map for playwright tests
-      (window as any).maplibreglMap = map;
-      (window as any).loadStatesCalled = true;
+      setMapInstance(map);
     });
 
-    mapRef.current = map;
     return () => map.remove();
   }, [onStateSelect]);
 
   return (
     <div className="relative w-full h-full">
       <div ref={mapContainer} className="absolute inset-0 w-full h-full" />
-      <Legend map={mapRef.current} />
+      <Legend map={mapInstance} />
     </div>
   );
 };
