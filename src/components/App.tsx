fa2da96757a458ab10ed7913ac1f4df822d978d8
Emma Tong <53305901+etong11@users.noreply.github.com>
2025-04-01 12:33:55 -0400

    fix: separated map component from app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,15 @@
-import maplibregl from 'maplibre-gl';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { CountyData, loadCounties } from './Counties';
-import Legend from './Legend';
+import { CountyData } from './Counties';
+import Map from './Map';
 import NavBar from './Navbar';
 import Sidebar from './Sidebar';
-import { loadStates, StateData, zoomToState } from './States';
+import { StateData, zoomToState } from './States';
 
 const App: React.FC = () => {
   const [selectedState, setSelectedState] = useState<StateData | null>(null);
   const [selectedCounty, setSelectedCounty] = useState<CountyData | null>(null);
+  const [mapInstance, setMapInstance] = useState<maplibregl.Map | null>(null);
 
   const handleStateSelect = useCallback((data: StateData) => {
     setSelectedState(data);
@@ -26,40 +26,9 @@ const App: React.FC = () => {
     setSelectedCounty(null);
   }, []);
 
-  const mapContainer = useRef<HTMLDivElement | null>(null);
-  const [mapInstance, setMapInstance] = useState<maplibregl.Map | null>(null);
-  const STYLE_VERSION = 8; // Required for declaring a style, may change in the future
-
-  useEffect(() => {
-    if (!mapContainer.current) {
-      return;
-    }
-    const API_KEY = process.env.MAPTILER_API_KEY;
-    const map = new maplibregl.Map({
-      container: mapContainer.current,
-      style: {
-        version: STYLE_VERSION,
-        sources: {},
-        layers: [],
-        glyphs: `https://api.maptiler.com/fonts/{fontstack}/{range}.pbf?key=${API_KEY}`,
-      },
-      center: [0, 0],
-      zoom: 4.2,
-      maxBounds: [
-        [-25, -15], // Southwest corner, Mainland USA
-        [25, 15], // Northeast corner, Mainland USA
-      ],
-    });
-
-    map.on('load', () => {
-      // Load states and pass the onStateSelect callback so a state click will notify the parent.
-      loadCounties(map, handleCountySelect);
-      loadStates(map, handleStateSelect);
-      setMapInstance(map);
-    });
-
-    return () => map.remove();
-  }, [handleStateSelect]);
+  const handleSetMapInstance = useCallback((map: maplibregl.Map) => {
+    setMapInstance(map);
+  }, []);
 
   return (
     <div>
@@ -75,13 +44,12 @@ const App: React.FC = () => {
           onClose={handleSidebarClose}
         />
         <div className="w-full h-full">
-          <div className="relative w-full h-full">
-            <div
-              ref={mapContainer}
-              className="absolute inset-0 w-full h-full"
-            />
-            <Legend map={mapInstance} />
-          </div>
+          <Map
+            onStateSelect={handleStateSelect}
+            onCountySelect={handleCountySelect}
+            mapInstance={mapInstance}
+            onMapSet={handleSetMapInstance}
+          />
         </div>
       </div>
     </div>
---
+++ src/components/Map.tsx
@@ -0,0 +1,64 @@
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import React, { useEffect, useRef } from 'react';
+import '../styles/index.css';
+import { CountyData, loadCounties } from './Counties';
+import Legend from './Legend';
+import { loadStates, StateData } from './States';
+
+interface MapProps {
+  onStateSelect?: (data: StateData) => void;
+  onCountySelect?: (data: CountyData) => void;
+  mapInstance: maplibregl.Map | null;
+  onMapSet: React.Dispatch<React.SetStateAction<maplibregl.Map | null>>;
+}
+
+const Map: React.FC<MapProps> = ({
+  onStateSelect,
+  onCountySelect,
+  mapInstance,
+  onMapSet,
+}) => {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const STYLE_VERSION = 8; // Required for declaring a style, may change in the future
+
+  useEffect(() => {
+    if (!mapContainer.current) {
+      return;
+    }
+    const API_KEY = process.env.MAPTILER_API_KEY;
+    const map = new maplibregl.Map({
+      container: mapContainer.current,
+      style: {
+        version: STYLE_VERSION,
+        sources: {},
+        layers: [],
+        glyphs: `https://api.maptiler.com/fonts/{fontstack}/{range}.pbf?key=${API_KEY}`,
+      },
+      center: [0, 0],
+      zoom: 4.2,
+      maxBounds: [
+        [-25, -15], // Southwest corner, Mainland USA
+        [25, 15], // Northeast corner, Mainland USA
+      ],
+    });
+
+    map.on('load', () => {
+      // Load states and pass the onStateSelect callback so a state click will notify the parent.
+      loadCounties(map, onCountySelect);
+      loadStates(map, onStateSelect);
+      onMapSet(map);
+    });
+
+    return () => map.remove();
+  }, [onStateSelect, onCountySelect, onMapSet]);
+
+  return (
+    <div className="relative w-full h-full">
+      <div ref={mapContainer} className="absolute inset-0 w-full h-full" />
+      <Legend map={mapInstance} />
+    </div>
+  );
+};
+
+export default Map;
